257c7ff7f15bb4c9e3780bd1aa1893353d10c1e4
Shafrin <shafrinsulthan03@gmail.com>
2026-06-26 22:45:45 +0530

    api created for payslip management

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ import authRouter from "./routes/authRoutes.js";
 import employeesRouter from "./routes/EmployeeRoutes.js";
 import ProfileRouter from "./routes/ProfileRoutes.js";
 import attendanceRouter from "./routes/attendanceRouter.js";
+import leaveRouter from "./routes/leaveRouter.js";
+import payslipRouter from "./routes/payslipsRouter.js";
 
 const app = express()
 const PORT = process.env.PORT || 4000;
@@ -24,6 +26,9 @@ app.use("/api.auth",authRouter)
 app.use("/api.employees",employeesRouter)
 app.use("/api.profile",ProfileRouter)
 app.use("/api.attendance",attendanceRouter)
+app.use("/api.leave",leaveRouter)
+app.use("/api.payslips",payslipRouter)
+
 
 
 
