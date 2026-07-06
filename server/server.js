c6435e2c237adb7cbf8f0687a32e067c894ac4a4
ShafrinSulthan <shafrinmunavarsulthan@gmail.com>
2026-07-06 23:40:37 +0530

    Fix cors

--- server/server.js
+++ server/server.js
@@ -19,7 +19,15 @@ const PORT = process.env.PORT || 4000;
 
 //Middleware
 
-app.use(cors())
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://ems-six-brown.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json())
 app.use(multer().none())
 
